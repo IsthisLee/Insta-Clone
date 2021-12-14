4d7a296cd3492d90a828ebe6985a3cf40adcbcf6
KIMHYEONGJIN5925 <93527060+KIMHYEONGJIN5925@users.noreply.github.com>
2021-12-14 20:59:14 +0900

    first commit
    first commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,127 @@
 const express = require("express");
 const router = express.Router();
+const Users = require("../../models/users");
+const Posts = require("../../models/posts");
+const jwt = require("jsonwebtoken");
+const secretKey = require("../../config/secretKey").secretKey;
+// const authMiddleware = require("../middlewares/auth-middleware");
+
+//회원가입(중복검사 + 규칙체크)
+router.post("/userlist/register", async (req, res) => {
+  try {
+    const { userId, nickname, password, passwordCheck } = req.body;
+
+    // 아이디: `알파벳 대소문자(a~z, A~Z), 숫자(0~9) 및 .뒤에는 2-3글자`로 구성
+    let checkId = userId.search(
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/g
+    );
+    if (checkId === -1) {
+      res.status(400).send({
+        errorMessage: "입력하신 아이디가 이메일 형식에 맞지 않습니다.",
+      });
+      return;
+    }
+
+    // 아이디 중복검사
+    const existUserId = await Users.findOne({
+      $or: [{ userId }],
+    });
+    if (existUserId) {
+      res.status(400).send({
+        errorMessage: "중복된 아이디입니다.",
+      });
+      return;
+    }
+
+    // 닉네임 : `길이는 4~15, 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자(-_)`로 구성
+    let checkNickname = nickname.search(/^[A-za-z0-9_-]{4,15}$/g);
+    if (checkNickname === -1) {
+      // console.log("아이디 형식 체크를 스쳐지나간다")
+      res.status(400).send({
+        errorMessage: "아이디가 형식에 맞지 않습니다.",
+      });
+      return;
+    }
+
+    //닉네임 중복검사
+    const existNickname = await Users.findOne({
+      $or: [{ nickname }],
+    });
+    if (existNickname) {
+      res.status(400).send({
+        errorMessage: "중복된 닉네임입니다.",
+      });
+      return;
+    }
+
+    // 비밀번호는 `최소 4자 이상이며, 닉네임과 같은 값이 포함된 경우 회원가입에 실패`
+    let checkPassword = password;
+    checkIncludeId = userId.split("@")[0];
+    if (password.length < 4 || checkPassword.includes(checkIncludeId)) {
+      res.status(400).send({
+        errorMessage:
+          "패스워드는 최소 4자 이상이며, 아이디와 같은 값은 포함될 수 없습니다.",
+      });
+      return;
+    }
+
+    //패스워드 불일치(패스워드 != 패스워드확인란)
+    if (password !== passwordCheck) {
+      res.status(400).send({
+        errorMessage: "패스워드가 패스워드 확인란과 동일하지 않습니다.",
+      });
+      return;
+    }
+
+    //유저 등록
+    const user = new Users({ userId, nickname, password });
+    await user.save();
+    res.status(201).send({ result: "success" });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      errorMessage: "요청한 데이터 형식이 올바르지 않습니다.22",
+    });
+    return;
+  }
+});
+
+//로그인
+
+router.post("/userlist/login", async (req, res) => {
+  try {
+    const { userId, password } = req.body; // 로그인정보 받음
+    const userFind = await Users.findOne({ userId, password }).exec();
+    if (!userFind) {
+      res.status(400).send({
+        errorMessage: "아이디 또는 패스워드가 잘못되었습니다.",
+      });
+      return;
+    }
+    const token = jwt.sign({ userId: userFind.userId }, secretKey); //토큰생성
+    console.log(`토큰생성완료 : ${token}`);
+
+    res.send({
+      token,
+      result: "success",
+    });
+  } catch (error) {
+    res.status(400).send({
+      errorMessage: "요청한 데이터 형식이 올바르지 않습니다.",
+    });
+  }
+});
+
+//유저 프로필 페이지
+router.get("/userpforile/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const userPosts = await Posts.find({ userId }).sort("-createdAt");
+    res.json({ userPosts });
+  } catch (error) {
+    res.render(`유저 프로필 페이지 에러`);
+  }
+});
 
 module.exports = router;
---
+++ models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose); // userId sequence 처리
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.plugin(AutoIncrement, { inc_field: "userIdCnt" });
+
+module.exports = mongoose.model("Users", userSchema);
