5afda8abf344a3066b495fa6265580272132df24
IsthisLee <rjsgmldnwn@gmail.com>
2021-12-15 12:14:13 +0900

    [건희] 게시글  업로드 API 통합

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,3 +1,4 @@
+const { Route53Resolver } = require("aws-sdk");
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
@@ -5,100 +6,135 @@ const Posts = require("../../models/posts");
 //const Comments = require("../../models/comments");
 const upload = require("../middlewares/multer");
 
-router.post("/upload", async (req, res) => {
-  //multer로 사진파일 업로드
-  try {
-    upload(req, res, (err) => {
-      if (err) {
-        return res.json({ success: false, err });
-      }
-      return res.json({ success: true, filePath: req.file.path });
-    });
-  } catch {
-    res.status(400).json({
-      errorMessage: "파일 업로드 오류 발생",
-    });
-  }
-});
-
 router
-  .route("/postlist")
+    .route("/postlist")
 
-  //메인 페이지 요청 API
-  .get(async (req, res) => {
-    try {
-      const postList = await Posts.find().sort("posrId");
-      //const commentList = await Comments.find().sort("commentId");
-      res.json({ postlist: postList, commentlist: "추가예정" });
-    } catch {
-      res.status(400).json({
-        errorMessage: "메인페이지 데이터 요청 오류 발생",
-      });
-    }
-  })
+    //메인 페이지 요청 API
+    .get(async (req, res) => {
+        try {
+            const postList = await Posts.find().sort("-postId");
+            //const commentList = await Comments.find().sort("-commentId");
 
-  //게시글 업로드 API
-  .post(async (req, res) => {
-    //사진 외 정보들 DB에 저장.(사진 경로 포함)
-    try {
-      const { userId, nickname, imgUrl, content } = req.body;
-      await Posts.create({ userId, nickname, imgUrl, content });
-      res.send();
-    } catch {
-      res.status(400).json({
-        errorMessage: "게시글 업로드 오류 발생",
-      });
-    }
-  });
-
-router
-  .route("/postlist/:postId")
-  .put(async (req, res) => {
-    const { postId } = req.params;
-    const { userId, content, imgUrl } = req.body;
-
-    try {
-      const post = await Posts.findOne({ postId: postId });
-      if (post.userId !== userId) {
-        return res.status(412).json({
-          errorMessage: "본인 게시글만 수정 가능합니다.",
-        });
-      }
-      await Posts.updateOne(
-        { postId: postId },
-        {
-          $set: {
-            content: content,
-            imgUrl: imgUrl,
-            updatedAt: Date(),
-          },
+            res.json({ postlist: postList, commentlist: "추가예정" });
+        } catch {
+            res.status(400).json({
+                errorMessage: "메인페이지 데이터 요청 오류 발생",
+            });
         }
-      );
-      res.send();
-    } catch {
-      res.status(400).json({
-        errorMessage: "게시글 수정 중 오류 발생",
-      });
-    }
-  })
-  .delete(async (req, res) => {
-    const { postId } = req.params;
-    const { userId } = req.body;
+    })
 
-    try {
-      const post = await Posts.findOne({ postId: postId });
-      if (post.userId !== userId) {
-        return res.status(412).json({
-          errorMessage: "본인 게시글만 삭제 가능합니다.",
+    //게시글 업로드 API
+    .post(async (req, res) => {
+        const userId = "rjsgmldnwn@gmail.com";
+
+        upload(req, res, async (err) => {
+            const { nickname, content } = req.body;
+
+            try {
+                const imgUrl = req.file.path;
+
+                if (err) {
+                    return res.json({ success: false, err });
+                }
+                await Posts.create({ userId, nickname, imgUrl, content });
+                res.send();
+            } catch {
+                res.status(400).json({
+                    errorMessage: "게시글 업로드 오류 발생",
+                });
+            }
         });
-      }
-      await Posts.deleteOne({ postId: postId });
-      res.send();
-    } catch {
-      res.status(400).json({
-        errorMessage: "게시글 삭제 중 오류 발생",
-      });
-    }
-  });
+    });
+
+router
+    .route("/postlist/:postId")
+
+    //게시글 수정 API
+    .put(async (req, res) => {
+        const { postId } = req.params;
+        const post = await Posts.findOne({ postId: postId });
+        const userId = "rjsgmldnwn@gmail.com";
+
+        if (post.userId !== userId) {
+            return res.status(412).json({
+                errorMessage: "본인 게시글만 수정 가능합니다.",
+            });
+        }
+
+        upload(req, res, async (err) => {
+            const { content } = req.body;
+            let imgUrl;
+
+            if (req.file) {
+                imgUrl = req.file.path;
+            } else imgUrl = post.imgUrl;
+
+            try {
+                if (err) {
+                    return res.json({ success: false, err });
+                }
+                await Posts.updateOne(
+                    { postId: postId },
+                    {
+                        $set: {
+                            content: content,
+                            imgUrl: imgUrl,
+                            updatedAt: Date(),
+                        },
+                    }
+                );
+                res.send();
+            } catch {
+                res.status(400).json({
+                    errorMessage: "게시글 수정 중 오류 발생",
+                });
+            }
+        });
+    })
+
+    //게시글 삭제 API
+    .delete(async (req, res) => {
+        const { postId } = req.params;
+        const userId = "rjsgmldnwn@gmail.com";
+
+        try {
+            const post = await Posts.findOne({ postId: postId });
+
+            if (post.userId !== userId) {
+                return res.status(412).json({
+                    errorMessage: "본인 게시글만 삭제 가능합니다.",
+                });
+            }
+            await Posts.deleteOne({ postId: postId });
+            res.send();
+        } catch {
+            res.status(400).json({
+                errorMessage: "게시글 삭제 중 오류 발생",
+            });
+        }
+    });
+
+//초안. 파일 업로드 API 따로 구성하기. => 게시글 API에 하나로 합쳐버림
+
+// router.post("/upload", async (req, res) => {
+//     //multer로 사진파일 업로드
+//     try {
+//         upload(req, res, (err) => {
+//             if (err) {
+//                 return res.json({ success: false, err });
+//             }
+//             return res.json({
+//                 success: true,
+//                 filePath: req.file.path,
+//                 body: req.body,
+//                 file: req.file,
+//             });
+//         });
+//     } catch {
+//         res.status(400).json({
+//             errorMessage: "파일 업로드 오류 발생",
+//         });
+//     }
+// });
 
 module.exports = router;
